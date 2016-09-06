9e703946e8eff9e77cc83c8f13b5835e41049412
ksskarthik628 <ksskarthik628@gmail.com>
2016-09-06 15:49:27 +0530

    learning Three.js

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+app.listen(port, ipaddress);
