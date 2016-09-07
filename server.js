fbb912ff96316c9ce24e317e2c7bd1cd869ebbb5
ksskarthik628 <ksskarthik628@gmail.com>
2016-09-07 14:17:10 +0530

    Chapter 1 code

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static(__dirname + '/public'));
+
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
